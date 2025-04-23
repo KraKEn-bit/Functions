04b02c1af51a318f575a11a78bd90b26cc5e817d
KraKEn-bit <rktaaseen@gmail.com>
2025-04-24 03:12:47 +0600

    Adding cpp code for test

--- test.cpp
+++ test.cpp
@@ -1,9 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <algorithm>
 #include<vector>
+#include<limits.h>
 
 using namespace std;
 
 int main(){
-    cout<<"Hello My name is Rafsan. It's a test project to learn github";
-    return 0;
+ int n;
+ cin>>n;
+ int rem=0,copy=n,count=0;
+ int islucky=0;
+ for(int i=1;i<=n;i++){
+    int temp = i; 
+    islucky = 1;  
+    while(temp != 0){
+        rem = temp % 10;
+        temp = temp / 10;
+        if(rem != 4 && rem != 7){
+            islucky = 0;
+            break;
+        }
+    }
+    if(islucky && n % i == 0){
+        count++;
+    }
+}
+if(count>=1){
+    cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+ return 0;
 }
